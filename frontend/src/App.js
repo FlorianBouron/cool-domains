f761b45b88133c55ac23dc21fff51b39cd86b833
Florian Bouron <bouron.florian@yahoo.fr>
2022-02-24 22:21:12 +0100

    Add switch network button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { ethers } from "ethers";
 import contractAbi from './utils/contractAbi.json';
+import polygonLogo from './assets/polygonlogo.png';
+import ethLogo from './assets/ethlogo.png';
+import { networks } from './utils/networks';
 import './styles/App.css';
 
 const tld = '.lambomoon';
@@ -10,6 +13,7 @@ const App = () => {
 	const [currentAccount, setCurrentAccount] = useState('');
 	const [domain, setDomain] = useState('');
 	const [record, setRecord] = useState('');
+	const [network, setNetwork] = useState('');
 
 	const connectWallet = async () => {
 		try {
@@ -43,6 +47,16 @@ const App = () => {
 			setCurrentAccount(account);
 		} else {
 			console.log('No authorized account found');
+		}
+
+		const chainId = await ethereum.request({ method: 'eth_chainId' });
+		setNetwork(networks[chainId]);
+
+		ethereum.on('chainChanged', handleChainChanged);
+
+		// Reload the page when they change networks
+		function handleChainChanged(_chainId) {
+			window.location.reload();
 		}
 	}
 
@@ -91,6 +105,47 @@ const App = () => {
 		}
 	}
 
+	const switchNetwork = async () => {
+		if (window.ethereum) {
+			try {
+				// Try to switch to the Mumbai testnet
+				await window.ethereum.request({
+					method: 'wallet_switchEthereumChain',
+					params: [{ chainId: '0x13881' }],
+				});
+			} catch (error) {
+				// This error code means that the chain we want has not been added to MetaMask
+				// In this case we ask the user to add it to their MetaMask
+				if (error.code === 4902) {
+					try {
+						await window.ethereum.request({
+							method: 'wallet_addEthereumChain',
+							params: [
+								{
+									chainId: '0x13881',
+									chainName: 'Polygon Mumbai Testnet',
+									rpcUrls: ['https://rpc-mumbai.maticvigil.com/'],
+									nativeCurrency: {
+										name: "Mumbai Matic",
+										symbol: "MATIC",
+										decimals: 18
+									},
+									blockExplorerUrls: ["https://mumbai.polygonscan.com/"]
+								},
+							],
+						});
+					} catch (error) {
+						console.log(error);
+					}
+				}
+				console.log(error);
+			}
+		} else {
+			// If window.ethereum is not found then MetaMask is not installed
+			alert('MetaMask is not installed. Please install it to use this app: https://metamask.io/download.html');
+		}
+	}
+
 	// This runs our function when the page loads.
 	useEffect(() => {
 		checkIfWalletIsConnected();
@@ -106,6 +161,14 @@ const App = () => {
 	);
 
 	const renderInputForm = () => {
+		if (network !== 'Polygon Mumbai Testnet') {
+			return (
+				<div className="connect-wallet-container">
+					<h2>Please switch to Polygon Mumbai Testnet</h2>
+					<button className='cta-button mint-button' onClick={switchNetwork}>Click here to switch</button>
+				</div>
+			);
+		}
 		return (
 			<div className="form-container">
 				<div className="first-row">
@@ -126,11 +189,8 @@ const App = () => {
 				/>
 
 				<div className="button-container">
-					<button className='cta-button mint-button' disabled={null} onClick={mintDomain}>
+					<button className='cta-button mint-button' onClick={mintDomain}>
 						Mint
-					</button>
-					<button className='cta-button mint-button' disabled={null} onClick={null}>
-						Set data
 					</button>
 				</div>
 
@@ -148,6 +208,10 @@ const App = () => {
 						<div className="left">
 							<p className="title">🌕 Cool Domains</p>
 							<p className="subtitle">Your immortal API on the blockchain!</p>
+						</div>
+						<div className="right">
+							<img alt="Network logo" className="logo" src={network.includes("Polygon") ? polygonLogo : ethLogo} />
+							{currentAccount ? <p> Wallet: {currentAccount.slice(0, 6)}...{currentAccount.slice(-4)} </p> : <p> Not connected </p>}
 						</div>
 					</header>
 				</div>
